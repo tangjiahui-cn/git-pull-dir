f1ff11a0b1f5b301139f84b027984ee129515201
tangjiahui <tjhaaa@163.com>
2026-07-22 21:14:31 +0800

    add config files and gitignore

---
+++ vitest.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    testTimeout: 10000,
+    hookTimeout: 30000,
+    include: ['test/**/*.test.ts'],
+  },
+});
